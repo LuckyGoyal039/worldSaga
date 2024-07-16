d1be04588aab23d86d371d23521b39c0d84d571f
Lucky Goyal <goyallucky2020@gmail.com>
2024-07-16 16:35:03 +0530

    add footer and logo

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,25 +1,39 @@
 
 import Image from "next/image"
-import mainLogo from '../../assets/images/mainLogo.webp'
+// import mainLogo from '/image/ws.png'
 import './header.css'
 import { RxHamburgerMenu } from "react-icons/rx";
+import Link from "next/link";
 export default function Header({ toggleSidebar }: any) {
     return (
         <div className="headerMain border-red-500">
-            <div>
-                <RxHamburgerMenu onClick={toggleSidebar} />
+            <div className="header-left">
+                {/* <div>
+                    <RxHamburgerMenu onClick={toggleSidebar} />
+                </div> */}
+                <div className="mainLogoContainer">
+                    <Link href={'/'}>
+                        <Image
+                            src={'/image/ws.png'}
+                            alt="Main Logo"
+                            objectFit="cover"
+                            width={100}
+                            height={100}
+                        />
+                    </Link>
+                </div>
+                <Link href={'/'}>
+                    <h1 className="worldSaga">WorldSaga</h1>
+                </Link>
             </div>
-            <div>
-                <a href='/home'>Home</a>
-                <a href='/trending'>Trending</a>
+            <div className="header-right">
+                <Link href={'/our-story'}>Our Story</Link>
+                <Link href={'/sign-in'}>Membership</Link>
+                <Link href={'/sign-in'}>Write</Link>
+                <Link href={'/sign-in'}>Sign in</Link>
+                <Link href={'/sign-in'}>Get Started</Link>
             </div>
-            <div className="mainLogoContainer">
-                <Image
-                    src={mainLogo}
-                    alt="Main Logo"
-                    className="mianLogoImage"
-                />
-            </div>
+
         </div>
     )
 }
