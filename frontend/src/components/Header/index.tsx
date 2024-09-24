de0cc9405bcfd419dcff40bb2467d186cc51af7b
Lucky Goyal <goyallucky2020@gmail.com>
2024-09-24 22:38:55 +0530

    common header responsiveness

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,37 +3,60 @@ import Image from "next/image"
 // import mainLogo from '/image/ws.png'
 import './header.css'
 import { RxHamburgerMenu } from "react-icons/rx";
+import DragHandleOutlinedIcon from '@mui/icons-material/DragHandleOutlined';
 import Link from "next/link";
 export default function Header({ toggleSidebar }: any) {
     return (
-        <div className="headerMain border-red-500">
-            <div className="header-left">
-                {/* <div>
+        <div className="headerOuter">
+            <div className="headerMain border-red-500">
+                <div className="header-left">
+                    {/* <div>
                     <RxHamburgerMenu onClick={toggleSidebar} />
                 </div> */}
-                <div className="mainLogoContainer">
+                    <div className="mainLogoContainer">
+                        <Link href={'/'}>
+                            <Image
+                                src={'/image/ws.png'}
+                                alt="Main Logo"
+                                objectFit="cover"
+                                width={30}
+                                height={30}
+                            />
+                        </Link>
+                    </div>
                     <Link href={'/'}>
-                        <Image
-                            src={'/image/ws.png'}
-                            alt="Main Logo"
-                            objectFit="cover"
-                            width={100}
-                            height={100}
-                        />
+                        <h3 className="worldSaga">WorldSaga</h3>
                     </Link>
                 </div>
-                <Link href={'/'}>
-                    <h1 className="worldSaga">WorldSaga</h1>
-                </Link>
-            </div>
-            <div className="header-right">
-                <Link href={'/our-story'}>Our Story</Link>
-                <Link href={'/sign-in'}>Membership</Link>
-                <Link href={'/sign-in'}>Write</Link>
-                <Link href={'/sign-in'}>Sign in</Link>
-                <Link href={'/sign-in'}>Get Started</Link>
-            </div>
+                <div className="header-right">
+                    <div className="hidden md:block">
+                        <Link href={'/our-story'}>Our Story</Link>
+                    </div>
+                    <div className="hidden md:block">
 
+                        <Link href={'/sign-in'}>Membership</Link>
+                    </div>
+                    <div className="hidden md:block">
+
+                        <Link href={'/sign-in'}>Write</Link>
+                    </div>
+                    <div className="hidden md:block">
+
+                        <Link href={'/sign-in'}>Get Started</Link>
+                    </div>
+                    <div className="loginDiv">
+                        <Link href={'/sign-in'}>Log in</Link>
+                        <span>L</span>
+                    </div>
+                    <div className="singupDiv">
+                        <Link href={'/sign-in'}>Sign up</Link>
+                    </div>
+                    <div className="md:hidden">
+                        <DragHandleOutlinedIcon />
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
