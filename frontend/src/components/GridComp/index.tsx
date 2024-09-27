e349cd2a61ab74671620ddfb4117dd1e8b6e7820
Lucky Goyal <goyallucky2020@gmail.com>
2024-09-27 22:04:15 +0530

    flex options styling

--- frontend/src/components/GridComp/index.tsx
+++ frontend/src/components/GridComp/index.tsx
@@ -15,46 +15,49 @@ export default function GridComp() {
 
             <hr className={styles.hrLine}></hr>
 
-            <div className={`${styles.mainGrid} ${styles.basicGrid}`}>
+            <div className={styles.mainFlex}>
+                <div className={styles.firstContainer}>
+                    <div>
+                        <Image src={GreenImage} alt="Green" width={300} height={300} className={styles.imageGreen} />
+                    </div>
+                    <div className={styles.content}>
+                        <div className={`${styles.textCenter}`}>
+                            <small className={`${styles.textColor} ${styles.mydate} `}>07.24.2020</small>
+                        </div>
+                        <div>
+                            <h1 className={`${styles.textColor} ${styles.morningTea} ${styles.textCenter} `}>Morning Tea</h1>
+                        </div>
+                        <div className={`${styles.textCenter} ${styles.width50}`}>
+                            <span >
+                                <small>
+                                    Lorem ipsum dolor sit amet consectetur adipiscing elic,sed do eirsmod tempor incididunt ut labore et dolore
+                                </small>
+                            </span>
+                        </div>
+                        <div>
+                            <small className={`${styles.readMore} ${styles.textColor}`}>Read More</small>
+                        </div>
+                    </div>
+                </div>
                 <div>
-                    <Image src={GreenImage} alt="Green" width={300} height={300} className={styles.imageGreen} />
-                </div>
-                <div className={styles.content}>
-                    <div className={`${styles.textCenter}`}>
-                        <small className={`${styles.textColor} ${styles.mydate} `}>07.24.2020</small>
-                    </div>
                     <div>
-                        <h1 className={`${styles.textColor} ${styles.morningTea} ${styles.textCenter} `}>Morning Tea</h1>
+                        <Image src={RedImage} alt="Red" width={360} height={360} className={styles.imageRed} />
                     </div>
-                    <div className={`${styles.textCenter} ${styles.width50}`}>
-                        <span >
-                            <small>
-                                Lorem ipsum dolor sit amet consectetur adipiscing elic,sed do eirsmod tempor incididunt ut labore et dolore
-                            </small>
-                        </span>
-                    </div>
-                    <div>
-                        <small className={`${styles.readMore} ${styles.textColor}`}>Read More</small>
-                    </div>
-                </div>
-
-                <div>
-                    <Image src={RedImage} alt="Red" width={360} height={360} className={styles.imageRed} />
-                </div>
-                <div className={styles.content}>
-                    <div><small className={`${styles.textColor} ${styles.mydate}`}>07.24.2020</small></div>
-                    <div>
-                        <h1 className={`${styles.textColor} ${styles.morningTea}`}>something else</h1>
-                    </div>
-                    <div>
-                        <span>
-                            <small>
-                                Lorem ipsum dolor sit amet consectetur adipiscing elic,sed do eirsmod tempor incididunt ut labore et dolore
-                            </small>
-                        </span>
-                    </div>
-                    <div>
-                        <small className={`${styles.readMore} ${styles.textColor}`}>Read More</small>
+                    <div className={styles.content}>
+                        <div><small className={`${styles.textColor} ${styles.mydate}`}>07.24.2020</small></div>
+                        <div>
+                            <h1 className={`${styles.textColor} ${styles.morningTea}`}>something else</h1>
+                        </div>
+                        <div>
+                            <span>
+                                <small>
+                                    Lorem ipsum dolor sit amet consectetur adipiscing elic,sed do eirsmod tempor incididunt ut labore et dolore
+                                </small>
+                            </span>
+                        </div>
+                        <div>
+                            <small className={`${styles.readMore} ${styles.textColor}`}>Read More</small>
+                        </div>
                     </div>
                 </div>
             </div>
